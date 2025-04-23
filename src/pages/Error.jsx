fb186b57917881610fac37a3232e675293e33e5e
Md. Rahad Ullah <rahadullah10@gmail.com>
2025-04-23 22:47:10 +0600

    Implemented error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,9 +1,18 @@
+import { Link } from "react-router";
+import Navbar from "../components/shared/Navbar";
+
 const Error = () => {
   return (
-    <div className="text-center flex flex-col gap-4 justify-center items-center min-h-screen">
-      <h1 className="text-5xl font-bold">404</h1>
-      <h3 className="text-xl font-medium">Oops! Page Not Found</h3>
-    </div>
+    <>
+      <Navbar />
+      <div className="text-center flex flex-col gap-4 justify-center items-center min-h-[90vh]">
+        <h1 className="text-5xl font-bold">404</h1>
+        <h3 className="text-xl font-medium">Oops! Page Not Found</h3>
+        <Link to={"/"} className="btn btn-primary rounded-full">
+          Go Home
+        </Link>
+      </div>
+    </>
   );
 };
 
